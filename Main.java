106dbf74f01fe13a45e086e820471aea1f208283
aidansun525 <42677795+aidansun525@users.noreply.github.com>
2018-10-28 20:45:03 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,9 +3,9 @@ public class Main {
     public static void main(String[] args)
     {
         double d1=calcD(25,15,23,20);
-        double d2=calcD(23,20,24,21);
-        double d3=calcD(24,21,27,20);
-        double d4=calcD(27,20,19,35);
+        double d2=calcD(23,20,21,24);
+        double d3=calcD(21,24,20,27);
+        double d4=calcD(20,27,19,35);
         double d5=calcD(19,35,21,46);
         double d6=calcD(21,46,23,56);
         double d7=calcD(23,56,25,65);
@@ -15,14 +15,14 @@ public class Main {
         double d11=calcD(34,74,36,74);
         double d12=calcD(36,74,39,74);
 
-        double totalDistance=d1+d2+d3+d4+d5+d6+d7+d8+d9+d10+d11+d12;
-        System.out.printf("Total Distance is %,.2f km.",totalDistance);
+        double totalDistance=d1 + d2 + d3 + d4 + d5 + d6 + d7 + d8 + d9 + d10 + d11 + d12;
+        System.out.printf("Total Distance is %,.1f km.",totalDistance);
     }
 
     public static double calcD(double x1, double y1, double x2, double y2)
     {
-        double a = Math.sin((x1-x2)/2)*Math.sin((x1-x2)/2)+ Math.cos(x1)* Math.cos(x2)* Math.sin((y1-y2)/2)*Math.sin((y1-y2)/2);
-        double c = 2* Math.atan2(Math.pow(a,1/2),Math.pow(1-a,1/2));
+        double a = Math.pow(Math.sin((x1-x2)/2),2)+ Math.cos(x1)* Math.cos(x2)* Math.pow(Math.sin((y1-y2)/2),2);
+        double c = 2* Math.atan2(Math.sqrt(a),Math.sqrt(1-a));
         double r = 6371;
         double d = r* c;
         return d;
